2a1a87d2b9f5323450b344447e4463459ab256f2
Allan Denot <adenot@gmail.com>
2012-10-20 16:16:14 +1100

    Migration for content table (pages and blocks)
    Signed-off-by: Allan Denot <adenot@gmail.com>

--- bundles/laranja/migrations/2012_10_20_041339_create_content_table.php
+++ bundles/laranja/migrations/2012_10_20_041339_create_content_table.php
@@ -9,7 +9,12 @@ class Laranja_Create_Content_Table {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('laranja_content', function($table) {
+			$table->string('path')->primary();
+			$table->string('storage_id', 64)->unique();
+			
+			$table->foreign('storage_id')->references('id')->on('laranja_storage');
+		});
 	}
 
 	/**
@@ -19,7 +24,7 @@ class Laranja_Create_Content_Table {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('laranja_content');
 	}
 
 }
